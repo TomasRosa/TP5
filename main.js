1e92cf2088edda1ef1227c30c9842a10537c9fef
TomasRosa <tomilautialdosivi@gmail.com>
2023-09-13 21:10:53 -0300

    que capo sos

--- main.js
+++ main.js
@@ -1,24 +1,69 @@
-document.addEventListener('DOMContentLoaded', () => 
-{
-const table = document.getElementById("table");
+document.addEventListener("DOMContentLoaded",() =>{
+    //Obtengo los ID de las tablas para luego manipularlas y rellenarlas.
+    const idEmpleado = document.getElementById('idEmpleado');
+    const idCompañia = document.getElementById('idCompañia');
+    const idNombre = document.getElementById('idNombre');
+    const idApellido = document.getElementById('idApellido');
+    const idEmail = document.getElementById('idEmail');
+    const idNombreCompañia = document.getElementById('idNombreCompañia');
+    const table = document.getElementById('table');
 
-/*Preguntar a Nahue si puedo intercambiar los backticks por la concatenacion*/ 
-fetch("https://jsonplaceholder.typicode.com/users") /*Consumo la api*/ 
-.then(res => res.json()) /*Paso la api a JSON.*/ 
-.then((users) =>{        /*Imprimo la data de la JSON y la retorno, preguntar por que.*/ 
-    console.log(users);
-    users.forEach(user =>{
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-        <td> ${user.name}</td>
-        <td> ${user.email}</td>
-        <td> ${user.username}</td>
-        <td>${user.company.name}</td>`
-        table.appendChild(tr);
-        /*Preguntar como acceder a las companys. Lo estoy haciendo con otra API xq la del TP no funciona. */ 
-    });
-})
-.catch(err =>{
-    console.error("Ha ocurrido un error. ", err);
-})
+    let empleados;
+    let compañia;
+
+    //Obtengo los endpoint de los empleados y la compañia a partir de la URL de la API.
+    const urlApi = "https://utn-lubnan-api-1.herokuapp.com";
+    const employeeEndpoint = `${urlApi}/api/Employee`;
+    const companyEndpoint = `${urlApi}/api/Company`;
+
+
+    fetch(employeeEndpoint)
+    .then(res => res.json())
+    .then(data =>{
+        empleados = data;
+        console.log(empleados); //Preguntar a nahue porque no puedo imprimirlo mediante backTicks
+        //Una vez que ya tengo los datos de los empleados, necesito los de la Company ya que los debo listar donde cada empleado tenga asignada su company correspondiente.
+        //Para realizar eso, tengo que hacer un fetch del endPoint de la company para luego pasarlo a json y asi poder utilizar esos datos en conjunto.
+        return fetch(companyEndpoint)
+    })
+    .then(resCompany => resCompany.json())
+    .then(data =>{
+        compañia = data;
+        console.log(data);
+        empleados.forEach(empleado =>{
+            const newRow = table.insertRow();
+
+            const tdIdEmpleado = newRow.insertCell();
+            tdIdEmpleado.innerHTML = empleado.employeeId;
+            
+            const tdCompany = newRow.insertCell();
+            tdCompany.innerHTML = empleado.companyId;
+            
+            const tdNombre = newRow.insertCell();
+            tdNombre.innerHTML = empleado.firstName; 
+            
+            const tdApellido = newRow.insertCell();
+            tdApellido.innerHTML = empleado.lastName; 
+            
+            const tdEmail = newRow.insertCell();
+            tdEmail.innerHTML = empleado.email;
+
+            let nombre = asignarCompañiaAEmpleado(empleado.companyId,compañia); ///Paso el id compañia ya que es el que comparten, estaba pasando el employee nada que evr
+
+            const tdNombreCompañia = newRow.insertCell();
+            tdNombreCompañia.innerHTML = nombre;
+        })
+    })
+    function asignarCompañiaAEmpleado (IDcompañia, arregloCompañia)
+    {
+        let retorno = null;
+
+        arregloCompañia.forEach(compañia =>{
+            if(IDcompañia === compañia.companyId)
+            {
+                retorno = compañia.name;
+            }
+        })
+        return retorno;
+    }
 })
